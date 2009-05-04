beefdedf94c2de9387cb5c8aeb5c5ec8550a98f9
Frank Golf <sha1-e884805f6c312aa2407c5005ebbb36cbb0c23396@cern.ch>
2009-05-04 21:26:19 +0000

    updated to use muon/tcmet value maps

--- python/TCMET_cfi.py
+++ python/TCMET_cfi.py
@@ -6,20 +6,27 @@ import FWCore.ParameterSet.Config as cms
 #
 # Form Track Corrected MET
 
-tcMet = cms.EDProducer(
-    "METProducer",
+tcMet = cms.EDProducer("METProducer",
     src = cms.InputTag("towerMaker"), #This parameter does not get used for TCMET
     METType = cms.string('TCMET'),
     alias = cms.string('TCMET'),
     noHF = cms.bool(False),
     globalThreshold = cms.double(0.0),
     InputType = cms.string('CaloMET:Electron:Muon:Track'),  #This parameter does not get used for TCMET  
-    electronLabel = cms.InputTag("pixelMatchGsfElectrons"),
-    muonLabel     = cms.InputTag("goodMuonsforMETCorrection"),
-    trackLabel    = cms.InputTag("generalTracks"),
-    metLabel      = cms.InputTag("corMetGlobalMuons"),
-    beamSpotLabel = cms.InputTag("offlineBeamSpot")
-    )
+    electronInputTag  = cms.InputTag("pixelMatchGsfElectrons"),
+    muonInputTag      = cms.InputTag("muons"),
+    trackInputTag     = cms.InputTag("generalTracks"),
+    metInputTag       = cms.InputTag("met"),
+    beamSpotInputTag  = cms.InputTag("offlineBeamSpot"),
+    muonDepValueMap   = cms.InputTag("muonMETValueMapProducer"  , "muCorrData"),     
+    tcmetDepValueMap  = cms.InputTag("muonTCMETValueMapProducer", "muCorrData"), 
+    pt_min  = cms.double(2.0),
+    pt_max  = cms.double(100.),
+    eta_max = cms.double(2.4), 
+    chi2_max = cms.double(4),
+    nhits_min = cms.double(11),
+    d0_max = cms.double(0.1)      
+)
 
 
 
